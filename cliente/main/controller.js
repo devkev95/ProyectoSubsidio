98da7626a72f33d559f96a104ea12e9442d160ff
kaguilar94m <kaguilar94m@gmail.com>
2016-10-28 13:14:56 -0600

    controller cambios

--- cliente/main/controller.js
+++ cliente/main/controller.js
@@ -1,4 +1,7 @@
 angular.module('myApp.controllers', []).controller('TablaProveedoresCtrl', function($scope, ProveedorChar){
 
-	BreakinBadChar.delete({"num_registro":"12121-55","nombreProveedor":"Z","direccion":"Otra cosa","correoContacto":"algoyotracosa@algo.com","estado":false,"cuentas":[{"numCuenta":"0006789-567-67"} ], "telefono":"2577-7777"});
+
+	$scope.characters = ProveedorChar.query();
+
+
 });
