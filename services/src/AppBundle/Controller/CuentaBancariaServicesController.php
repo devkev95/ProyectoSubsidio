1ddd4e739cae569db969834569519681af6a04b3
devkev95 <devkev95@gmail.com>
2016-10-28 09:44:56 -0600

    Arreglo problemas

--- services/src/AppBundle/Controller/CuentaBancariaServicesController.php
+++ services/src/AppBundle/Controller/CuentaBancariaServicesController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
 use AppBundle\Entity\CuentaBancaria;
 
-class CuentaBancariaServicesControllers extends FOSRestController
+class CuentaBancariaServicesController extends FOSRestController
 {
 	
 	/**
