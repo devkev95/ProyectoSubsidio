5797f56976623d9045047988382a3c4b4dc0b0a0
kaguilar94m <kaguilar94m@gmail.com>
2016-11-23 10:20:48 -0600

    presupuesto anual (Solo archivos)

---
+++ services/src/AppBundle/Entity/PresupuestoAnual.php
@@ -0,0 +1,70 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PresupuestoAnualRepository")
+ * @ORM\Table(name="presupuestoAnual")
+ */
+class CuentaBancaria
+{
+	/**
+	* @ORM\Column(type="string", length=10, name="año")
+	* @ORM\Id
+	*/
+	private $año;
+
+	/**
+	* @ORM\Column(type="float", name="monto")
+	*/
+	private $monto;
+
+    /**
+     * Set año
+     *
+     * @param string $año
+     *
+     * @return PresupuestoAnual
+     */
+    public function setAño($año)
+    {
+        $this->año = $año;
+
+        return $this;
+    }
+
+    /**
+     * Get año
+     *
+     * @return string
+     */
+    public function getAño()
+    {
+        return $this->año;
+    }
+
+   
+    /**
+     * Set montoActual
+     *
+     * @param float $monto
+     *
+     * @return PresupuestoAnual
+     */
+    public function setMonto($monto)
+    {
+        $this->monto= $monto;
+
+        return $this;
+    }
+
+    /**
+     * Get monto
+     *
+     * @return float
+     */
+    public function getMonto()
+    {
+        return $this->monto;
+    }
+}
